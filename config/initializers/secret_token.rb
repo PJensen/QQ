a90fa4f5491d2a8675ac1f7b3f1aa3c0b8057dfa
PJensen <jensen.petej@gmail.com>
2011-07-28 18:28:52 -0400

    'QQ'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QQ::Application.config.secret_token = '1fd42f9c03e5b0051df41acd51266641084fa2ec43d4b0fe1b59a2273a90d9930cfabbbb92bb10a0f70cdbe55df736653c883aa03b12f86e828055c924b4e928'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+QQ::Application.config.session_store :cookie_store, :key => '_QQ_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# QQ::Application.config.session_store :active_record_store
